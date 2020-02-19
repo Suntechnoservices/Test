a0627993d25dd7c5fa62634f58b5b4ee7ff5793e
PraveenTeza <praveennandan@suntechnoservices.com>
2020-02-19 22:35:42 +0530

    This is my second commit

--- src/com/suntechnoservices/vedikas/test/Test.java
+++ src/com/suntechnoservices/vedikas/test/Test.java
@@ -1,10 +1,41 @@
 package com.suntechnoservices.vedikas.test;
 
+/*
+* The Test project implements an application that
+* simply displays prime numbers upto 20 to the standard output.
+*
+* @author  Praveen Kumar
+* @version 1.0
+* @since   2020-02-19 
+*/
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Github")
+		
+		 int i =0;
+	       int num =0;
+	       //Empty String
+	       String  primeNumbers = "";
+
+	       for (i = 1; i <= 20; i++)         
+	       { 		  	  
+	          int counter=0; 	  
+	          for(num =i; num>=1; num--)
+		  {
+	             if(i%num==0)
+		     {
+	 		counter = counter + 1;
+		     }
+		  }
+		  if (counter ==2)
+		  {
+		     //Appended the Prime number to the String
+		     primeNumbers = primeNumbers + i + " ";
+		  }	
+	       }	
+	       System.out.println("Prime numbers from 1 to 20 are :");
+	       System.out.println(primeNumbers);
 	}
 
 }
